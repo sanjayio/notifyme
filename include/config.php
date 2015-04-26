018116c7824a0325b5810bc7e9b8ecadd5f58cd6
Sanjay Kumar A <sanjaykumara1994@gmail.com>
2015-04-26 12:05:43 +0530

    Add config and database connection script

---
+++ include/config.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "notifyme");
+define("DB_PASSWORD", "jarvis123");
+define("DB_DATABASE", "notifyme");
+?>
